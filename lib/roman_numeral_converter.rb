1174880bf21c3cda519fe290fa21416964a19169
Peter Melling <pjmelling@gmail.com>
2016-02-18 22:30:04 -0600

    Enhance converter yet again

--- lib/roman_numeral_converter.rb
+++ lib/roman_numeral_converter.rb
@@ -2,8 +2,30 @@ require "roman_numeral_converter/version"
 
 module RomanNumeralConverter
   class Converter
-    def convert(i)
-      'I' * i
+    def convert(n)
+      result = ''
+
+      while n >= 10
+        result << 'X'
+        n -= 10
+      end
+
+      if n >= 9
+        result << 'IX'
+        n -= 9
+      end
+
+      if n >= 5
+        result << 'V'
+        n -= 5
+      end
+
+      if n >= 4
+        result << 'IV'
+        n -= 4
+      end
+
+      result << 'I' * n
     end
   end
 end
--- test/roman_numeral_converter_test.rb
+++ test/roman_numeral_converter_test.rb
@@ -1,17 +1,43 @@
 require 'test_helper'
 
 class RomanNumeralConverterTest < Minitest::Test
+  def setup
+    @converter = RomanNumeralConverter::Converter.new
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::RomanNumeralConverter::VERSION
   end
 
   def test_it_converts_1_to_I
-    converter = RomanNumeralConverter::Converter.new
-    assert_equal converter.convert(1), 'I'
+    assert_equal 'I', @converter.convert(1)
   end
 
   def test_it_converts_2_to_II
-    converter = RomanNumeralConverter::Converter.new
-    assert_equal converter.convert(2), 'II'
+    assert_equal 'II', @converter.convert(2)
+  end
+
+  def test_it_converts_4_to_IV
+    assert_equal 'IV', @converter.convert(4)
+  end
+
+  def test_it_converts_5_to_V
+    assert_equal 'V', @converter.convert(5)
+  end
+
+  def test_it_converts_6_to_VI
+    assert_equal 'VI', @converter.convert(6)
+  end
+
+  def test_it_converts_9_to_IX
+    assert_equal 'IX', @converter.convert(9)
+  end
+
+  def test_it_converts_10_to_X
+    assert_equal 'X', @converter.convert(10)
+  end
+
+  def test_it_converts_20_to_XX
+    assert_equal 'XX', @converter.convert(20)
   end
 end
