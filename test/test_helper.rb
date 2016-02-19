b735d218712ce1904484e0dcfd01fc75a0cede64
Peter Melling <pjmelling@gmail.com>
2016-02-18 21:36:16 -0600

    Use minitest-reporters for colored output

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'roman_numeral_converter'
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
